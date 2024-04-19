4f4ecc14d2f53321ea946b08d9f9e0b8907d9d0d
EnzoPierucci <enzopierucci@gmail.com>
2024-04-19 16:36:35 +0200

    Restaur and type

--- database/seeders/RestaurantsTableSeeder.php
+++ database/seeders/RestaurantsTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Restaurant;
 class RestaurantsTableSeeder extends Seeder
 {
     /**
@@ -12,6 +12,12 @@ class RestaurantsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Restaurant::create([
+            'name' => 'NomeRistorante',
+            'slug' => 'slug-ristorante',
+            'address' => 'Indirizzo del ristorante',
+            'piva' => '01234567890',
+            'user_id' => 1, // Sostituisci con l'ID dell'utente proprietario del ristorante
+        ]);
     }
 }
--- database/seeders/TypesTableSeeder.php
+++ database/seeders/TypesTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Type;
 class TypesTableSeeder extends Seeder
 {
     /**
@@ -12,6 +12,10 @@ class TypesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Type::create([
+            'name' => 'Tipo di ristorante',
+            'slug' => 'tipo-di-ristorante',
+            'image' => 'percorso/dell/immagine.jpg',
+        ]);
     }
 }
