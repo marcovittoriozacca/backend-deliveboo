2ff04b2471896947a683e1b54cff4c087adf0394
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-04-19 16:05:23 +0200

    Add orders migration

--- database/migrations/2024_04_19_133132_create_orders_table.php
+++ database/migrations/2024_04_19_133132_create_orders_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->varchar("full_name");
+            $table->varchar("email");
+            $table->varchar("address");
+            $table->varchar("tel_number");
+            $table->text("description");
+            $table->date("date");
+            $table->bool("status")->default(0);
+            $table->decimal("total_price");
+            $table->unsignedBigInteger('restaurant_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('restaurant_id');
+            $table->foreign('restaurant_id')->references('id')->on('restaurants');
+
+
+
+
             $table->timestamps();
         });
     }
