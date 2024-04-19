95113f119daa9dd1f3683d76db016cb5170e9bff
marcovittoriozacca <zaccamarco01@gmail.com>
2024-04-19 16:18:52 +0200

    feat: Eloquent relationship between Users, restaurants and types

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Restaurant extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "name",
+        "address",
+        "piva",
+        "image",
+        "user_id",
+    ];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo( User::class );
+    }
+
+    public function types(): BelongsToMany{
+        return $this->belongsToMany( Type::class );
+    }
 }
