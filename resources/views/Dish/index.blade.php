26480091d3122e2dd0cc9198c730df755bf48694
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-04-20 19:24:02 +0200

    create layout for restaurant

--- resources/views/Dish/index.blade.php
+++ resources/views/Dish/index.blade.php
@@ -1,18 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <main class="container-fluid index_main">
+    <main class="container-fluid  px-0 position-relative">
+
+            <div class="">
+                <figure class="mb-0 immagine_qualcosa" style="width: 99%">
+                    <img class="img-fluid blur w-100" src="/restaurant_bg_1.jpg" alt="a">
+                </figure>
+            </div>
 
 
-        <div class="container">
 
 
 
 
 
+
+
+
+
+
+        <div class="container-fluid position-absolute index_main card_block">
 
                     @foreach ($dishes as $dish)
-                <div class="row row-cols-1 row-cols-md-3 mx-3 mx-sm-5 ">
+                <div class="row row-cols-1 g-4 row-cols-md-5 mx-3 mx-sm-5">
 
                     <div class="col">
 
@@ -37,12 +48,8 @@
 
 
                                 <div class="d-flex flex-column gap-2">
-                                    <div class="button_edit rounded-3 text-center py-1">
-                                        <a class="text-decoration-none text-light " href="{{ route('dishes.edit', [$restaurant->id, $dish->id]) }}">EDIT</a>
-                                    </div>
-                                    <div class="button_delete rounded-3 text-center py-1">
-                                        <a class="text-decoration-none text-light " href="{{ route('dishes.edit', [$restaurant->id, $dish->id]) }}">REMOVE</a>
-                                    </div>
+                                    <a class="text-center w-100 rounded-3 button_edit text-decoration-none text-light py-1" href="{{ route('dishes.edit', [$restaurant->id, $dish->id]) }}">EDIT</a>
+                                    <a class="text-center button_delete w-100 rounded-3 button_edit text-decoration-none text-light py-1" href="{{ route('dishes.edit', [$restaurant->id, $dish->id]) }}">REMOVE</a>
                                 </div>
 
                         </div>
@@ -78,12 +85,8 @@
 
 
                                 <div class="d-flex flex-column gap-2">
-                                    <div class="button_edit rounded-3 text-center py-1">
-                                        <a class="text-decoration-none text-light " href="{{ route('dishes.edit', [$restaurant->id, $dish->id]) }}">EDIT</a>
-                                    </div>
-                                    <div class="button_delete rounded-3 text-center py-1">
-                                        <a class="text-decoration-none text-light " href="{{ route('dishes.edit', [$restaurant->id, $dish->id]) }}">REMOVE</a>
-                                    </div>
+                                        <a class="text-center w-100 rounded-3 button_edit text-decoration-none text-light py-1" href="{{ route('dishes.edit', [$restaurant->id, $dish->id]) }}">EDIT</a>
+                                        <a class="text-center button_delete w-100 rounded-3 button_edit text-decoration-none text-light py-1" href="{{ route('dishes.edit', [$restaurant->id, $dish->id]) }}">REMOVE</a>
                                 </div>
 
                         </div>
@@ -99,13 +102,9 @@
 
                 </div>
                     @endforeach
-
-
-
-
-                <div>
-                    <a href="{{ route('dishes.create', $restaurant->id) }}">CREA</a>
-                </div>
+                    <div>
+                        <a class="btn-base-orange p-3 rounded-4" href="{{ route('dishes.create', $restaurant->id) }}">AGGIUNGI PIATTO</a>
+                    </div>
 
     </main>
 
