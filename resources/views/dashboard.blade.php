477a72b29d23665debcde2df883d0945c8f8d263
marcovittoriozacca <zaccamarco01@gmail.com>
2024-04-20 17:07:15 +0200

    feat: dashboard layout starting point

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row">
+        <div class="col-12">
+            <h1>Benvenuto {{ Auth::user()->name }}</h1>
+            <h3>In questa pagina potrai gestire il tuo ristorante!</h3>
+        </div>
+        <div class="col-6 d-none d-lg-block">
+            <img class="img-fluid" src="" alt="dashboard-banner">
+        </div>
         @foreach ($restaurants as $restaurant)
         <div class="col-12">
             <p>{{ $restaurant->activity_name }}</p>
