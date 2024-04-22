1c924a76a6e26e7e9fa0515033da295fb5490045
erosalpe <eros.alpegiani@gmail.com>
2024-04-22 10:20:10 +0200

    feat: added storage:link/ fix: now dish.create accept .00 decimals

--- resources/views/Dish/create.blade.php
+++ resources/views/Dish/create.blade.php
@@ -88,7 +88,8 @@
                     <label for="price" class="form-label">Prezzo</label>
                     <input 
                         name="price" 
-                        type="number" 
+                        type="number"
+                        step=".01" 
                         class="form-control @error ('price') is-invalid @enderror" 
                         id="price"
                         required
