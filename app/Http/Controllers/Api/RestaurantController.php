dbe37dcc1c9aeb772c4bce0cc5c989929dd212e5
marcovittoriozacca <zaccamarco01@gmail.com>
2024-04-30 11:25:05 +0200

    fix: query now should be correct

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -35,9 +35,13 @@ class RestaurantController extends Controller
         //in questa abbiamo ogni ristorante con le tipologie associate, contenuto nell'array types passato da vue a laravel. Poi con una funzione anonima di php 
         //(simili a quelle di Js che iterano ogni elemento - whereHas), andiamo a iterare ogni elemento di types e prendiamo tutti i ristoranti che hanno almeno
         //una delle tipologie dichiarate nell'array types
-        $restaurants = Restaurant::with('types')->whereHas('types', function ($query) use ($types) {
-            $query->whereIn('slug', $types);
-        })->get();
+        $restaurants = Restaurant::with('types');
+        foreach ($types as $type) {
+            $restaurants->whereHas('types', function ($query) use ($type) {
+                $query->where('slug', $type);
+            });
+        }
+        $restaurants = $restaurants->get();
         return response()->json([
             'success' => true,
             'restaurants' => $restaurants,
