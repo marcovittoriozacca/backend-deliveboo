574cc1a2c31b00c91ea2cf50c6d449942e949440
erosalpe <eros.alpegiani@gmail.com>
2024-05-01 19:21:21 +0200

    ora il gravico visualizza solo gli ordini collegati al ristorante e non tutti gli ordini della tabella orders

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -25,7 +25,7 @@ class OrderController extends Controller
     }
 
 
-
+    /*
     public function ordersChart()
     {
         // Ottieni tutti gli ordini dal database
@@ -48,6 +48,31 @@ class OrderController extends Controller
         // Passa i dati alla vista
         return view('dish.orders_chart', compact('labels', 'data'));
     }
+    */
+
+    public function ordersChart()
+    {
+        // Ottieni tutti gli ordini collegati al ristorante dell'utente autenticato dal database
+        $orders = Order::where('restaurant_id', Auth::id())->get();
+        
+        // Raggruppa gli ordini per mese e anno e conta il numero di ordini
+        $ordersCount = $orders->groupBy(function ($order) {
+            return $order->created_at->format('Y-m'); // Chiave basata sul timestamp Unix
+        })->map(function ($group) {
+            return $group->count();
+        });
+
+        // Ordina le chiavi (timestamp Unix) in ordine cronologico
+        $ordersCount = $ordersCount->sortKeys();
+
+        // Estrai le etichette (mese/anno) e i dati (numero di ordini) dal conteggio
+        $labels = $ordersCount->keys();
+        $data = $ordersCount->values();
+
+        // Passa i dati alla vista
+        return view('dish.orders_chart', compact('labels', 'data'));
+    }
+
 
 
     /**
