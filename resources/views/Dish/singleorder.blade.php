82e7cfc621b7e57f23c1c24b32f710080b1805e2
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-05-07 10:31:58 +0200

    make order page more responsive

--- resources/views/Dish/singleorder.blade.php
+++ resources/views/Dish/singleorder.blade.php
@@ -11,20 +11,22 @@
             <h3>Email: {{$orders[0]->email}}</h3>
             <h3>Telefono: {{$orders[0]->tel_number}}</h3>
             <h4>Indirizzo: {{$orders[0]->address}}</h4>
+            <h4>Data: {{$orders[0]->date}}</h4>
+            <h4>Importo: {{$orders[0]->total_price}}€</h4>
         </div>
     </div>
 
     <div style="min-height:calc(100vh - 85.45px - 448px)">
 
         <div class="custom-table-container">
-            <div class="rounded w-50 mx-auto overflow-hidden dishes-container">
+            <div class="rounded mx-auto overflow-hidden dishes-container">
                 <div class="table-responsive w-100">
                     <table class="table table-warning mb-0">
                         <thead>
                             <tr>
-                    
+
                             <th scope="col" class="py-3 text-center" style="width: 200px">Nome</th>
-                    
+
                             <th scope="col" class="py-3 text-center" style="width: 200px">Quantità</th>
 
                             </tr>
